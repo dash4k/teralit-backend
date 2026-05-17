4d94efbad550519efc03407efe01931bd6a42aab
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-17 15:08:51 +0700

    added error middleware

---
+++ src/middlewares/error.js
@@ -0,0 +1,15 @@
+import response from '../utils/response.js';
+import ClientError from '../exceptions/client-error.js';
+
+const ErrorHandler = (err, _req, res) => {
+  if (err instanceof ClientError) return response(res, err.statusCode, err.message, null);
+  if (err.isJoi) return response(res, 400, err.details[0], null);
+
+  const status = err.statusCode || err.status || 500;
+  const message = err.message || 'Internal Server Error';
+
+  console.log('Unhandled Error', err);
+  return response(res, status, message, null);
+};
+
+export default ErrorHandler;
