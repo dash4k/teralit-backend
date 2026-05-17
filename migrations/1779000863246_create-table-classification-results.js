a29b1ed4c6855d2c2b017d5c525c51ca96044ea2
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-17 14:04:29 +0700

    added classification_results table migrations

---
+++ migrations/1779000863246_create-table-classification-results.js
@@ -0,0 +1,52 @@
+/* eslint-disable camelcase */
+
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('classification_results', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+      notNull: true,
+    },
+    session_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    diagnosis: {
+      type: 'VARCHAR(100)',
+      notNull: true,
+    },
+    confidence: {
+      type: 'REAL',
+      notNull: true,
+    },
+    details: {
+      type: 'JSONB',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TIMESTAMPTZ',
+      notNull: true,
+    },
+  });
+
+  pgm.createConstraint('classification_results', 'fk_classification_results.session_id_sessions.id', 'FOREIGN KEY(session_id) REFERENCES sessions(id) ON DELETE CASCADE');
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('classification_results');
+};
