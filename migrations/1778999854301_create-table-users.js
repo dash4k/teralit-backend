ff11ee7390798b2cd56534c0b3effbab1c0b244e
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-17 13:40:16 +0700

    added users table migration

---
+++ migrations/1778999854301_create-table-users.js
@@ -0,0 +1,51 @@
+/* eslint-disable camelcase */
+
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+      notNull: true,
+    },
+    email: {
+      type: 'VARCHAR(100)',
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TIMESTAMPTZ',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TIMESTAMPTZ',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('users');
+};
