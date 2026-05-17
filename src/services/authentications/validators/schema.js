ab402257f540cb15829e5b15aaf8168e7c8e6933
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-17 15:51:57 +0700

    added payload schemas for authentications

---
+++ src/services/authentications/validators/schema.js
@@ -0,0 +1,20 @@
+import Joi from 'joi';
+
+export const registerPayloadSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  name: Joi.string().required(),
+});
+
+export const loginPayloadSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+export const putAuthenticationPayloadSchema = Joi.object({
+  refreshToken: Joi.string().required(),
+});
+
+export const deleteAuthenticationPayloadSchema = Joi.object({
+  refreshToken: Joi.string().required(),
+});
