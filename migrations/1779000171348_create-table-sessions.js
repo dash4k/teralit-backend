6ac58214accb77a2e7aa699a2e10166ae823bb54
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-17 13:47:49 +0700

    added sessions table migration

---
+++ migrations/1779000171348_create-table-sessions.js
@@ -0,0 +1,50 @@
+/* eslint-disable camelcase */
+
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('sessions', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+      notNull: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    status: {
+      type: 'VARCHAR(20)',
+      notNull: true,
+      default: 'pending',
+      check: "status IN ('pending', 'completed', 'failed')",
+    },
+    created_at: {
+      type: 'TIMESTAMPTZ',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TIMESTAMPTZ',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('sessions', 'fk_sessions.user_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('sessions');
+};
