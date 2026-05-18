ac4769924d8d1224523be270a559427c3e42f0f8
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-18 12:24:43 +0700

    added server routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 
 import express from 'express';
+import ErrorHandler from '../middlewares/error.js';
+import routes from '../routes/index.js';
 
 const app = express();
-
 app.use(express.json());
+app.use(ErrorHandler);
+app.use(routes);
 
 export default app;
