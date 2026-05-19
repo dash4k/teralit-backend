1958596ce32b1045e407cd234435da0797d0d58d
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-19 20:36:14 +0700

    fixed middleware logical flow

--- src/server/index.js
+++ src/server/index.js
@@ -5,8 +5,9 @@ import ErrorHandler from '../middlewares/error.js';
 import routes from '../routes/index.js';
 
 const app = express();
+
 app.use(express.json());
-app.use(ErrorHandler);
 app.use(routes);
+app.use(ErrorHandler);
 
 export default app;
