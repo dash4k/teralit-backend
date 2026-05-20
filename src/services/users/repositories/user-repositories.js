3fb00017b8f79ef6fc9ba007e468cc770d4128a0
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-20 14:56:07 +0700

    chore: added user id verification

--- src/services/users/repositories/user-repositories.js
+++ src/services/users/repositories/user-repositories.js
@@ -5,6 +5,15 @@ class UserRepositories {
     this._pool = new Pool();
   }
 
+  async verifyUserId(id) {
+    const result = await this._pool.query({
+      text: 'SELECT id FROM users WHERE id = $1',
+      values: [id],
+    });
+
+    return result.rows.length > 0;
+  }
+
   async getUser(id) {
     const result = await this._pool.query({
       text: 'SELECT email, name, updated_at FROM users WHERE id = $1',
@@ -22,11 +31,15 @@ class UserRepositories {
     const updatedAt = new Date().toISOString();
 
     const result = await this._pool.query({
-      text: 'UPDATE users SET name = $1, updated_at = $2 WHERE id = $3',
+      text: 'UPDATE users SET name = $1, updated_at = $2 WHERE id = $3 RETURNING email, name, updated_at',
       values: [name, updatedAt, id],
     });
 
-    return result.rows[0];
+    return {
+      email: result.rows[0].email,
+      name: result.rows[0].name,
+      updatedAt: result.rows[0].updated_at,
+    };
   }
 
   async deleteUser(id) {
