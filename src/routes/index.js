4bc17449cfe61be2c43d64013447db0f5873b901
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-20 14:57:21 +0700

    chore: implemented user routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import authentications from '../services/authentications/routes/index.js';
+import users from '../services/users/routes/index.js';
 
 const router = Router();
 
 router.use('/', authentications);
+router.use('/', users);
 
 export default router;
