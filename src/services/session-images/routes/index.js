4b2c738696f22b5a10a9557f49e861e2081c423b
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-21 11:45:31 +0700

    fix: added mergeParams options to router

--- src/services/session-images/routes/index.js
+++ src/services/session-images/routes/index.js
@@ -6,7 +6,7 @@ import {
 import authenticateToken from '../../../middlewares/auth.js';
 import { upload } from '../storage/storage-config.js';
 
-const router = Router();
+const router = Router({ mergeParams: true });
 
 router.post('/images', authenticateToken, upload.single('image'), uploadImage);
 router.get('/images', authenticateToken, viewImage);
