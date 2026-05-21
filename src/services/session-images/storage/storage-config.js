2b271f8ff761722c438a6579ffae784b155f34e5
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-21 09:50:13 +0700

    chore: added storage config with multer

---
+++ src/services/session-images/storage/storage-config.js
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import path from 'path';
+import multer from 'multer';
+import ClientError from '../../../exceptions/client-error';
+
+export const UPLOAD_FOLDER = path.resolve('src/services/uploads/files/images');
+
+if (!fs.existsSync(UPLOAD_FOLDER)) {
+  fs.mkdirSync(UPLOAD_FOLDER, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, UPLOAD_FOLDER),
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedMimetypes = ['image/jpeg', 'image/png'];
+
+  if (allowedMimetypes.includes(file.mimetype)) cb(null, true);
+  else cb(new ClientError('Image is required'), null);
+};
+
+export const upload = multer({
+  storage,
+  limits: { fieldSize: 5 * 1024 * 1024 },
+  fileFilter: fileFilter
+});
+
+export const deleteFromDisk = async (filePath) => {
+  try {
+    await fs.promises.unlink(filePath);
+  } catch (error) {
+    console.log('Error while trying to delete the image: ', error.message);
+  }
+};
+
+export default { UPLOAD_FOLDER, storage, fileFilter, upload };
