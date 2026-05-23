0797b1397b0134992dca42bfb35db67225cab755
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-23 11:54:56 +0700

    chore: refactor /messages post route && removed /agents post route

--- src/services/messages/routes/index.js
+++ src/services/messages/routes/index.js
@@ -1,20 +1,17 @@
 import { Router } from 'express';
 import {
-  saveMessage,
   agentAnswer,
   listMessages
 } from '../controllers/message-controller.js';
 import authenticateToken from '../../../middlewares/auth.js';
 import validate from '../../../middlewares/validate.js';
 import {
-  postMessagePayloadSchema,
   postMessageToAgentPayloadSchema
 } from '../validators/schema.js';
 
 const router = Router({ mergeParams: true });
 
-router.post('/messages', authenticateToken, validate(postMessagePayloadSchema), saveMessage);
-router.post('/agents', authenticateToken, validate(postMessageToAgentPayloadSchema), agentAnswer);
+router.post('/messages', authenticateToken, validate(postMessageToAgentPayloadSchema), agentAnswer);
 router.get('/messages', authenticateToken, listMessages);
 
 export default router;
