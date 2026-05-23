3fb79385f2d640de9d00453b8026ffd5c4eff52f
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-23 11:55:20 +0700

    chore: removed postMessagePayloadSchema

--- src/services/messages/validators/schema.js
+++ src/services/messages/validators/schema.js
@@ -1,10 +1,5 @@
 import Joi from 'joi';
 
-export const postMessagePayloadSchema = Joi.object({
-  role: Joi.string().valid('user', 'agent').required(),
-  content: Joi.string().required(),
-});
-
 export const postMessageToAgentPayloadSchema = Joi.object({
   content: Joi.string().required(),
 });
