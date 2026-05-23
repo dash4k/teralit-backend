5884a17997b56db52bea938e2237b0b7d049bde2
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-23 14:24:59 +0700

    feat: added API documentation using swagger.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,8 @@ import sessions from '../services/sessions/routes/index.js';
 import sessionImages from '../services/session-images/routes/index.js';
 import classificationResults from '../services/classification-results/routes/index.js';
 import messages from '../services/messages/routes/index.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../../swagger-output.json' with { type: 'json' };
 
 const router = Router();
 
@@ -15,5 +17,6 @@ router.use('/', sessions);
 router.use('/sessions/:sessionId/', sessionImages);
 router.use('/sessions/:sessionId/', classificationResults);
 router.use('/sessions/:sessionId/', messages);
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 export default router;
