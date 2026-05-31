edcd9ce1e5ce68a11818ef326519683cdea87315
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-31 16:10:31 +0700

    feat: added logging in app

--- src/server/index.js
+++ src/server/index.js
@@ -3,12 +3,14 @@ import 'dotenv/config';
 import express from 'express';
 import ErrorHandler from '../middlewares/error.js';
 import routes from '../routes/index.js';
+import logger from '../middlewares/logger.js';
 import cors from 'cors';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 app.use(routes);
 app.use(ErrorHandler);
 
